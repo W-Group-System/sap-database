30d22909ffa1c658c61bbeeef06a5c78f5fd991e
xentra-ehipolito <emmanuel.hipolito@xentragroup.com>
2026-06-23 17:34:47 +0800

    update

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -79,7 +79,7 @@ class OrderController extends Controller
             'bdeName'  => function($bde) {
                 $bde->select('SlpName','SlpCode');
             }])
-            ->select('DocEntry', 'DocDate', 'DocNum', 'CardCode as Customer', 'NumAtCard as BuyersCode', 'CardName', 'U_Label', 'U_Packaging','SlpCode','U_Inco','U_PortDestination','U_CountryDen','U_Delivery','U_Onpallet','U_Modeship','U_PortLoad')
+            ->select('DocEntry', 'DocDate', 'DocNum', 'CardCode as Customer', 'NumAtCard as BuyersCode', 'CardName', 'U_Label', 'U_Packaging','SlpCode','U_Inco','U_PortDestination','U_CountryDen','U_Delivery','U_Onpallet','U_Modeship','U_PortLoad as LoadingPort')
             ->where('DocStatus', 'O');
 
             if (!empty($buyersCode)) {
@@ -228,7 +228,7 @@ class OrderController extends Controller
             'bdeName'  => function($bde) {
                 $bde->select('SlpName','SlpCode');
             }])
-            ->select('DocEntry', 'DocDate', 'DocNum', 'CardCode as Customer', 'NumAtCard as BuyersCode', 'CardName', 'U_Label', 'U_Packaging','SlpCode','U_Inco','U_CountryDen as U_PortDestination','U_CountryDen','U_Delivery','U_Onpallet','U_Modeship','U_PortLoad')
+            ->select('DocEntry', 'DocDate', 'DocNum', 'CardCode as Customer', 'NumAtCard as BuyersCode', 'CardName', 'U_Label', 'U_Packaging','SlpCode','U_Inco','U_CountryDen as U_PortDestination','U_CountryDen','U_Delivery','U_Onpallet','U_Modeship','U_Loadingport as LoadingPort')
             ->where('DocStatus', 'O');
 
             if (!empty($buyersCode)) {
@@ -376,7 +376,7 @@ class OrderController extends Controller
             'bdeName'  => function($bde) {
                 $bde->select('SlpName','SlpCode');
             }])
-            ->select('DocEntry', 'DocDate', 'DocNum', 'CardCode as Customer', 'NumAtCard as BuyersCode', 'CardName', 'U_Label', 'U_Packaging','SlpCode','U_Inco','U_CountryDen as U_PortDestination','U_CountryDen','U_Delivery','U_Onpallet','U_Modeship','U_PortLoad')
+            ->select('DocEntry', 'DocDate', 'DocNum', 'CardCode as Customer', 'NumAtCard as BuyersCode', 'CardName', 'U_Label', 'U_Packaging','SlpCode','U_Inco','U_CountryDen as U_PortDestination','U_CountryDen','U_Delivery','U_Onpallet','U_Modeship','U_Loadingport as LoadingPort')
             ->where('DocStatus', 'O');
 
             if (!empty($buyersCode)) {
