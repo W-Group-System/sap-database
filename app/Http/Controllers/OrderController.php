d55e820c67e6ebfb63e43908b21c084382dce3f3
xentra-ehipolito <emmanuel.hipolito@xentragroup.com>
2026-06-23 10:18:09 +0800

    update

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -228,7 +228,7 @@ class OrderController extends Controller
             'bdeName'  => function($bde) {
                 $bde->select('SlpName','SlpCode');
             }])
-            ->select('DocEntry', 'DocDate', 'DocNum', 'CardCode as Customer', 'NumAtCard as BuyersCode', 'CardName', 'U_Label', 'U_Packaging','SlpCode','U_Inco','U_PortDestination','U_CountryDen')
+            ->select('DocEntry', 'DocDate', 'DocNum', 'CardCode as Customer', 'NumAtCard as BuyersCode', 'CardName', 'U_Label', 'U_Packaging','SlpCode','U_Inco','U_CountryDen as U_PortDestination','U_CountryDen')
             ->where('DocStatus', 'O');
 
             if (!empty($buyersCode)) {
@@ -376,7 +376,7 @@ class OrderController extends Controller
             'bdeName'  => function($bde) {
                 $bde->select('SlpName','SlpCode');
             }])
-            ->select('DocEntry', 'DocDate', 'DocNum', 'CardCode as Customer', 'NumAtCard as BuyersCode', 'CardName', 'U_Label', 'U_Packaging','SlpCode','U_Inco','U_PortDestination','U_CountryDen')
+            ->select('DocEntry', 'DocDate', 'DocNum', 'CardCode as Customer', 'NumAtCard as BuyersCode', 'CardName', 'U_Label', 'U_Packaging','SlpCode','U_Inco','U_CountryDen as U_PortDestination','U_CountryDen')
             ->where('DocStatus', 'O');
 
             if (!empty($buyersCode)) {
